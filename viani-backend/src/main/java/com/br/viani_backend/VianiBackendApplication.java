32eae8aeca5211114fee07ec3aedc91e06588793
Davi Tavares <dxrktavares3@gmail.com>
2026-03-21 13:06:49 -0300

    feat: adiciona estrutura base do backend do projeto

---
+++ viani-backend/src/main/java/com/br/viani_backend/VianiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.viani_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VianiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VianiBackendApplication.class, args);
+	}
+
+}
---
+++ viani-backend/src/test/java/com/br/viani_backend/VianiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.viani_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VianiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
